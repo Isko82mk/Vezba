272dc98661652f794ca4c0ae649a063afceb08a3
ISKO J <janevski.ivica@yahoo.com>
2020-03-06 22:36:31 +0100

    Add project files.

---
+++ iskoVezbaCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace iskoVezbaCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+  
+            string num1, num2, oper;
+
+            Console.WriteLine("Enter first num");
+            num1 = Console.ReadLine();
+            double doubleNum1 = Convert.ToDouble(num1);
+
+            Console.WriteLine("Enter operator(+-*/)");
+            oper = Console.ReadLine();
+
+
+            Console.WriteLine("Enter sec num");
+            num2 = Console.ReadLine();
+            double doubleNum2 = Convert.ToDouble(num2);
+
+            if (oper == "+") Console.WriteLine($" The result is => {doubleNum1 + doubleNum2}");
+            if (oper == "-") Console.WriteLine($" The result is => {doubleNum1 - doubleNum2}");
+            if (oper == "*") Console.WriteLine($" The result is => {doubleNum1 * doubleNum2}");
+            if (oper == "/") Console.WriteLine($" The result is => {doubleNum1 / doubleNum2}");
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
