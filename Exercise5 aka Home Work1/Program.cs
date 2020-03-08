76874ab7e0052752eeb5864f1639215e2a3b1503
Ivica janevski <janevski.ivica@outlook.com>
2020-03-08 14:57:54 +0100

    nameChange

---
+++ Exercise5 aka Home Work1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise5_aka_Home_Work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
---
+++ vezba2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace vezba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+         
+
+            Console.WriteLine("Enter first num");
+            double doublefirst = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second num");
+            double doubleSecond = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter third num");
+            double doubleThird = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter forth num");
+            double doubleForth = Convert.ToDouble(Console.ReadLine());
+
+            double averageNumber = (doublefirst + doubleSecond + doubleForth + doubleThird) / 4;
+
+            Console.WriteLine( $"The average number is => {averageNumber}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
